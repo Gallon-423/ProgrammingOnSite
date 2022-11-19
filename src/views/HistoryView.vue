652bd7a648637e4a01428014b1d6094adc043fd8
phoshowy <2892393166@qq.com>
2022-11-19 15:57:51 +0800

    add:实现了历史记录框

---
+++ src/views/HistoryView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+import Onehistory from '@/components/Onehistory.vue';
+const props = defineProps<{records:}>
+</script>
+
+<template> 
+<div class="div2">
+    <Onehistory origin="111111111" newone="一千一百一万一十一" />
+    <Onehistory origin="1" newone="一千一百一万一十一" />
+    <Onehistory origin="111111111" newone="一千一十一" />
+ <!--
+    <Onehistory v-for="item in history"
+          :origin="item['title']"
+          :newone="item['description']"
+           />
+ --> 
+</div>
+</template>
+<style>
+.div2{
+    display: flex;
+    align-items: center;
+    justify-items: center;
+    background-color: aliceblue;
+    flex-direction: column;
+}
+</style>
